07284de9fa1d494a3e109ced8be6cc33ff3a810b
Alex <alexvgutnik@gmail.com>
2022-10-11 14:51:36 +0200

    models 1.0

--- network/models.py
+++ network/models.py
@@ -1,26 +1,60 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 
 class SNUser(models.Model):
-    pass
+    user = models.OneToOneField(User)
+    photo = models.ImageField(upload_to='uploads/avatars/', null=True)
+    phone_number = models.CharField(max_length=12)
+    videos = models.ManyToManyField('Video', null=True)
+    likes = models.ForeignKey('Likes', null=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
 
 
 class Video(models.Model):
-    pass
+    video = models.FileField(upload_to='uploads/videos')
+    heading = models.CharField(max_length=128)
+    text = models.TextField(max_length=1024)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+    likes = models.ManyToManyField('Like', null=True)
+
+    def __str__(self):
+        return f'{self.heading}'
+
 
 
 class Comment(models.Model):
-    pass
+    text = models.TextField(max_length=512)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    user = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.text}'
 
 
 class Subscription(models.Model):
-    pass
+    subscriber = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+    subscribed_to = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.subscriber} subscribed to {self.subscribed_to}'
 
 
 class Ban(models.Model):
-    pass
+    banned_by = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+    banned = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.banned_by} banned {self.banned}'
 
 
 class Like(models.Model):
-    pass
+    post = models.ForeignKey(Video, on_delete=models.CASCADE)
+    user = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user} liked post {self.post}'
