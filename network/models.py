bac33ab9e4c1c6f2c5f6a635340140129370c65b
Alex <alexvgutnik@gmail.com>
2022-10-12 17:38:53 +0200

    models 2.1

--- network/models.py
+++ network/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 
 class SNUser(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     photo = models.ImageField(upload_to='uploads/avatars/', null=True)
     phone_number = models.CharField(max_length=12)
 
-    videos = models.ManyToManyField('Video', null=True)
-    subscribtions = models.ManyToManyField('Subscription', null=True)
-    black_list = models.ManyToManyField('Ban', null=True)
-    likes = models.ManyToManyField('Like', null=True)
+    videos = models.ManyToManyField('Video')
+    subscribtions = models.ManyToManyField('Subscription')
+    black_list = models.ManyToManyField('Ban')
+    likes = models.ManyToManyField('Like')
 
     def __str__(self):
         return f'{self.user.username}'
@@ -24,8 +24,8 @@ class Video(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
 
     author = models.ForeignKey(SNUser, on_delete=models.CASCADE)
-    comments = models.ManyToManyField('Comment', null=True)
-    likes = models.ManyToManyField('Like', null=True)
+    comments = models.ManyToManyField('Comment', related_name="Comments")
+    likes = models.ManyToManyField('Like')
 
     def __str__(self):
         return f'{self.heading}'
@@ -34,33 +34,33 @@ class Video(models.Model):
 class Comment(models.Model):
     text = models.TextField(max_length=512)
     creation_date = models.DateTimeField(auto_now_add=True)
-    
-    video = models.ForeignKey(Video, on_delete=models.CASCADE)
-    user = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name="Video")
+    user = models.ForeignKey(SNUser, on_delete=models.CASCADE, related_name="Users")
 
     def __str__(self):
         return f'{self.text}'
 
 
 class Subscription(models.Model):
-    subscriber = models.ForeignKey(SNUser, on_delete=models.CASCADE)
-    subscribed_to = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+    subscriber = models.ForeignKey(SNUser, on_delete=models.CASCADE, related_name="Subscriber")
+    subscribed_to = models.ForeignKey(SNUser, on_delete=models.CASCADE, related_name="Subscribed_to")
 
     def __str__(self):
         return f'{self.subscriber} subscribed to {self.subscribed_to}'
 
 
 class Ban(models.Model):
-    banned_by = models.ForeignKey(SNUser, on_delete=models.CASCADE)
-    banned = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+    banned_by = models.ForeignKey(SNUser, on_delete=models.CASCADE, related_name="Banned_by")
+    banned = models.ForeignKey(SNUser, on_delete=models.CASCADE, related_name="Banned")
     
     def __str__(self):
         return f'{self.banned_by} banned {self.banned}'
 
 
 class Like(models.Model):
-    post = models.ForeignKey(Video, on_delete=models.CASCADE)
-    user = models.ForeignKey(SNUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(Video, on_delete=models.CASCADE, related_name="Post")
+    user = models.ForeignKey(SNUser, on_delete=models.CASCADE, related_name="User")
 
     def __str__(self):
         return f'{self.user} liked post {self.post}'
